28bb72ca716f94df101b7dc5fad7901971dbcb29
himanshu-96 <himanshuwarekar@yahoo.com>
2018-09-05 16:47:15 +0530

    forex.py

--- forex.py
+++ forex.py
@@ -4,11 +4,36 @@ import matplotlib.ticker as mticker
 import matplotlib.dates as mdates
 import numpy as np
 
-def percentchange(startPoint, currentPoint):
-    return ((currentPoint-startPoint)/startPoint)*100
+date,bid,ask = np.loadtxt("GBPUSD1d.txt", unpack=True, delimiter=",", converters={0:mdates.bytespdate2num('%Y%m%d%H%M%S')})
+
+def percentChange(startPoint, currentPoint):
+    return ((float(currentPoint)-float(startPoint))/float(startPoint))*100.00
+
+def patternFinder():
+    avgLine = ((bid+ask)/2)
+    x = len(avgLine)-30
+    y = 11
+    while y < x:
+        p1 = percentChange(avgLine[y-10], avgLine[y-9])
+        p2 = percentChange(avgLine[y-10], avgLine[y-8])
+        p3 = percentChange(avgLine[y-10], avgLine[y-7])
+        p4 = percentChange(avgLine[y-10], avgLine[y-6])
+        p5 = percentChange(avgLine[y-10], avgLine[y-5])
+        p6 = percentChange(avgLine[y-10], avgLine[y-4])
+        p7 = percentChange(avgLine[y-10], avgLine[y-3])
+        p8 = percentChange(avgLine[y-10], avgLine[y-2])
+        p9 = percentChange(avgLine[y-10], avgLine[y-1])
+        p10 = percentChange(avgLine[y-10], avgLine[y])
+        outcomeRange = avgLine[y+20:y+30]
+        currentPoint = avgLine[y]
+        value = functools.reduce(lambda x, y: x+y, outcomeRange) / len(outcomeRange)
+        print(value)
+        print(currentPoint)
+        print(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10)
+        y+=1
+        time.sleep(5555)
 
 def graphRawFX():
-    date,bid,ask = np.loadtxt("GBPUSD1d.txt", unpack=True, delimiter=",", converters={0:mdates.bytespdate2num('%Y%m%d%H%M%S')})
     fig = plt.figure(figsize=(10, 7))
     ax1 = plt.subplot2grid((40,40), (0,0), rowspan=50, colspan=50)
     ax1.plot(date,bid)
